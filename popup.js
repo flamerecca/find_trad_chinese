1cde2aae21d67723d5a862d4e24b2f1a9be5f3d7
recca chao <recca.chao@language-center.com.tw>
2019-02-16 18:28:43 +0800

    add function

--- popup.js
+++ popup.js
@@ -5,7 +5,12 @@
 'use strict';
 
 let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+
+changeColor.onclick = function(element) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            {
+                file: 'contentScript.js'
+            });
+    });
+};
